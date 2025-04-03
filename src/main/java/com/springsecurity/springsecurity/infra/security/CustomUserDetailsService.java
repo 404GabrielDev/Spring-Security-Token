3a00340ec4b5019cf14400f575986aaf76daee44
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-04-03 10:32:36 -0300

    feat(auth): implementar serviço de autenticação e repositório de usuários

--- src/main/java/com/springsecurity/springsecurity/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/springsecurity/springsecurity/infra/security/CustomUserDetailsService.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
--- src/main/java/com/springsecurity/springsecurity/repository/UserRepository.java
+++ src/main/java/com/springsecurity/springsecurity/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.springsecurity.springsecurity.repository;
 
 import com.springsecurity.springsecurity.domain.user.User;
+import io.jsonwebtoken.Claims;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.lang.ScopedValue;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
