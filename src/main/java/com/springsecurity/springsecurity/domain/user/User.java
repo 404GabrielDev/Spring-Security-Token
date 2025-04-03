1b5e572db9037b8466906d11d4cc9b6ecf9425e0
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-04-02 23:36:43 -0300

    feat: Implementando Entidade

---
+++ src/main/java/com/springsecurity/springsecurity/domain/user/User.java
@@ -0,0 +1,23 @@
+package com.springsecurity.springsecurity.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+}
