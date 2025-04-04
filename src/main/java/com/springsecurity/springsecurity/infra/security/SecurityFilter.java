fb3431a8ab1f7014801d7216bf64780824109123
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-04-04 00:23:29 -0300

    fix(DTOs): configurar o Security Config

--- src/main/java/com/springsecurity/springsecurity/infra/security/SecurityFilter.java
+++ src/main/java/com/springsecurity/springsecurity/infra/security/SecurityFilter.java
@@ -26,6 +26,14 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+
+        if(requestURI.equals("/auth/login") || requestURI.equals("/auth/register")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         var token = this.recoverToken(request);
         var login = tokenService.validateToken(token);
 
--- src/main/java/com/springsecurity/springsecurity/infra/security/TokenService.java
+++ src/main/java/com/springsecurity/springsecurity/infra/security/TokenService.java
@@ -2,6 +2,7 @@ package com.springsecurity.springsecurity.infra.security;
 
 import com.springsecurity.springsecurity.domain.user.User;
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
