f8ade13a5b97148e428d647279438bff95ac9556
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-04-04 00:24:12 -0300

    feat: implementar UserController e UserRepository

--- src/main/java/com/springsecurity/springsecurity/repository/UserRepository.java
+++ src/main/java/com/springsecurity/springsecurity/repository/UserRepository.java
@@ -1,10 +1,7 @@
 package com.springsecurity.springsecurity.repository;
 
 import com.springsecurity.springsecurity.domain.user.User;
-import io.jsonwebtoken.Claims;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.lang.ScopedValue;
 import java.util.Optional;
 import java.util.UUID;
 
