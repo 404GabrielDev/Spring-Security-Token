ae5633b39b2f3a6895a551ef8b64e8a3e5b95e02
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-04-04 00:21:56 -0300

    fix(controllers): atualizar metodos deprecateds

---
+++ src/main/java/com/springsecurity/springsecurity/controllers/AuthController.java
@@ -0,0 +1,56 @@
+package com.springsecurity.springsecurity.controllers;
+
+import com.springsecurity.springsecurity.domain.user.User;
+import com.springsecurity.springsecurity.dto.LoginRequestDTO;
+import com.springsecurity.springsecurity.dto.RegisterRequestDTO;
+import com.springsecurity.springsecurity.dto.ResponseDTO;
+import com.springsecurity.springsecurity.infra.security.TokenService;
+import com.springsecurity.springsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody LoginRequestDTO body) {
+        User user = this.userRepository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
+
+        if(passwordEncoder.matches(body.password(), user.getPassword())) {
+            String token = this.tokenService.generateToken(user);
+            return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body) {
+        Optional<User> user = this.userRepository.findByEmail(body.email());
+
+        if(user.isEmpty()) {
+            User newUser = new User();
+            newUser.setPassword(passwordEncoder.encode(body.password()));
+            newUser.setEmail(body.email());
+            newUser.setName(body.name());
+            this.userRepository.save(newUser);
+
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+}
