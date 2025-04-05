1f27de702e5c3e498df76e582ec2f55e85a129b4
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-04-04 23:36:23 -0300

    fix: corrigir o tipo de conteudo retornado

--- src/main/java/com/springsecurity/springsecurity/controllers/UserController.java
+++ src/main/java/com/springsecurity/springsecurity/controllers/UserController.java
@@ -5,11 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @GetMapping
-    public ResponseEntity<String> getUser() {
-        return ResponseEntity.ok("Sucesso!");
+    public ResponseEntity<Map<String, String>> getUser() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Sucesso!");
+        return ResponseEntity.ok(response);
     }
+
 }
