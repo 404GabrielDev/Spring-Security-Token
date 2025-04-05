30109b46fce8c275a26f3fcdfd8b9aea544b407b
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-04-04 23:55:57 -0300

    feat: configurar Rotas da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageProtectedComponent } from './components/page-protected/page-protected.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: RegisterComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'pag-protected',
+        component: PageProtectedComponent
+    }
+];
